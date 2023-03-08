0057e0b99c32ebd4b94307d9fc3054fcf38205b4
Wallacy <wlcsv@pop-os.localdomain>
2023-03-08 11:05:46 -0300

    First test passed

---
+++ test/globals.d.ts
@@ -0,0 +1,8 @@
+// declare namespace NodeJS {
+//     interface Global {
+//         testRequest: import('supertest').SuperTest<import('supertest').Test>;
+//     }
+// }
+
+declare var testRequest: import('supertest').SuperTest<import('supertest').Test>;
+
