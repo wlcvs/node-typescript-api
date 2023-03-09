d30c90e8dd80ac184e1273193081e764d6f42db3
Wallacy <wlcsv@pop-os.localdomain>
2023-03-09 11:28:49 -0300

    Mocks e mais algumas outras coisas

--- test/globals.d.ts
+++ test/globals.d.ts
@@ -4,5 +4,6 @@
 //     }
 // }
 
-declare var testRequest: import('supertest').SuperTest<import('supertest').Test>;
-
+declare var testRequest: import('supertest').SuperTest<
+  import('supertest').Test
+>;
